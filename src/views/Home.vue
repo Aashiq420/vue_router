4e5b0234f86927bc09bfecd45e0b944944811d99
Aashiq <adams.aashiq@gmail.com>
2021-04-20 09:01:35 +0200

    cheeky 420 morning commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore fuga corrupti hic iste, voluptate nam, itaque nisi distinctio voluptatibus ex officiis vero cum animi quidem dolores deserunt quisquam et repudiandae inventore mollitia dolorum quaerat optio? Labore deserunt sit dignissimos cum modi magnam fugit nisi dolor! Numquam doloribus tempora qui commodi molestias ipsum voluptas assumenda, dignissimos est aperiam exercitationem soluta autem nesciunt dolor quibusdam facere corporis temporibus quod aliquam? Saepe voluptates, magnam eos blanditiis molestias beatae incidunt dolorem illum provident corrupti tempore! Numquam totam facilis ea omnis nulla nesciunt quae quibusdam at quam earum. Illo ex molestias voluptates atque possimus natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore fuga corrupti hic iste, voluptate nam, itaque nisi distinctio voluptatibus ex officiis vero cum animi quidem dolores deserunt quisquam et repudiandae inventore mollitia dolorum quaerat optio? Labore deserunt sit dignissimos cum modi magnam fugit nisi dolor! Numquam doloribus tempora qui commodi molestias ipsum voluptas assumenda, dignissimos est aperiam exercitationem soluta autem nesciunt dolor quibusdam facere corporis temporibus quod aliquam? Saepe voluptates, magnam eos blanditiis molestias beatae incidunt dolorem illum provident corrupti tempore! Numquam totam facilis ea omnis nulla nesciunt quae quibusdam at quam earum. Illo ex molestias voluptates atque possimus natus!</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
